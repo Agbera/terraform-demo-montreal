460762cc015771b1e07515ef721aa99287966ccd
Agbera <126923646+Agbera@users.noreply.github.com>
2023-03-27 19:45:25 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,10 +7,7 @@ variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket-latest8999",
-    "mytestbucket8888",
-    "montrealcollege-demo",
-    "bucket-project-full-of-happiness"
+    "my-test-bucket-latest29292"
 
   ]
 }
